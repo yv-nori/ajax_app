d3d7dd8cdab850d492c4f27e18c03134b8b338a1
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-01-03 15:32:55 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
   def index # indexアクションを定義しました
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
-  def new
-
-  end
+  # 削除
+  # def new
+  # end
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
--- db/migrate/20201223034209_create_posts.rb
+++ db/migrate/20201223034209_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.text :content
+      t.boolean :cheched
       t.timestamps
     end
   end
